20a2f15b5aa733e59272e72a11811acf37be89ae
An Vuong <anvuongbk13@gmail.com>
2019-08-28 19:51:33 +0700

    fix monitor metrics

--- age_estimation/age_estimation_train.py
+++ age_estimation/age_estimation_train.py
@@ -231,29 +231,48 @@ def main():
     tensorboard_callback = TensorBoard(log_dir=str(logdir), update_freq=log_freq)
 
     # Initialize callbacks
-    monitor_metric = "val_age_mae"
     if args.center_loss == 1:
         monitor_metric = "val_pred_age_age_mae"
-
-    if opt_name is not 'adadelta':
-        callbacks = [LearningRateScheduler(schedule=Schedule(nb_epochs, initial_lr=lr)),
-                    ModelCheckpoint(str(weights_dir) + "/{}.".format(model_name) + "weights.{epoch:03d}-{val_loss:.3f}-{val_age_mae:.3f}" + "-{}.hdf5".format(dt_now),
-                                    monitor=monitor_metric,
-                                    verbose=1,
-                                    save_best_only=True,
-                                    save_weights_only=True,
-                                    mode="min"),
-                    tensorboard_callback
-                    ]
+        if opt_name is not 'adadelta':
+            callbacks = [LearningRateScheduler(schedule=Schedule(nb_epochs, initial_lr=lr)),
+                        ModelCheckpoint(str(weights_dir) + "/{}.".format(model_name) + "weights.{epoch:03d}-{val_pred_age_loss:.3f}-{val_pred_age_age_mae:.3f}" + "-{}.hdf5".format(dt_now),
+                                        monitor=monitor_metric,
+                                        verbose=1,
+                                        save_best_only=True,
+                                        save_weights_only=True,
+                                        mode="min"),
+                        tensorboard_callback
+                        ]
+        else:
+            callbacks = [ModelCheckpoint(str(weights_dir) + "/{}.".format(model_name) + "weights.{epoch:03d}-{val_pred_age_loss:.3f}-{val_pred_age_age_mae:.3f}" + "-{}.hdf5".format(dt_now),
+                                        monitor=monitor_metric,
+                                        verbose=1,
+                                        save_best_only=True,
+                                        save_weights_only=True,
+                                        mode="min"),
+                        tensorboard_callback
+                        ]
     else:
-        callbacks = [ModelCheckpoint(str(weights_dir) + "/{}.".format(model_name) + "weights.{epoch:03d}-{val_loss:.3f}-{val_age_mae:.3f}" + "-{}.hdf5".format(dt_now),
-                                    monitor=monitor_metric,
-                                    verbose=1,
-                                    save_best_only=True,
-                                    save_weights_only=True,
-                                    mode="min"),
-                    tensorboard_callback
-                    ]
+        monitor_metric = "val_age_mae"
+        if opt_name is not 'adadelta':
+            callbacks = [LearningRateScheduler(schedule=Schedule(nb_epochs, initial_lr=lr)),
+                        ModelCheckpoint(str(weights_dir) + "/{}.".format(model_name) + "weights.{epoch:03d}-{val_loss:.3f}-{val_age_mae:.3f}" + "-{}.hdf5".format(dt_now),
+                                        monitor=monitor_metric,
+                                        verbose=1,
+                                        save_best_only=True,
+                                        save_weights_only=True,
+                                        mode="min"),
+                        tensorboard_callback
+                        ]
+        else:
+            callbacks = [ModelCheckpoint(str(weights_dir) + "/{}.".format(model_name) + "weights.{epoch:03d}-{val_loss:.3f}-{val_age_mae:.3f}" + "-{}.hdf5".format(dt_now),
+                                        monitor=monitor_metric,
+                                        verbose=1,
+                                        save_best_only=True,
+                                        save_weights_only=True,
+                                        mode="min"),
+                        tensorboard_callback
+                        ]
 
     model.fit_generator(generator=train_gen,
                         epochs=nb_epochs,
